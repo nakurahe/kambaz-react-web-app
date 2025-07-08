fd64b1ccd73631ddbec1b9ce1fef837f2893e4a0
Nakura <126201955+nakurahe@users.noreply.github.com>
2025-07-08 22:43:08 +0900

    chore: add my section name and a few more dummy data in lab1 index

--- src/Labs/Lab1/index.tsx
+++ src/Labs/Lab1/index.tsx
@@ -118,7 +118,54 @@ export default function Lab1() {
                             <td>2/10/21</td>
                             <td>90</td>
                         </tr>
-                        <tr> ... </tr>
+                        <tr>
+                            <td>Q3</td>
+                            <td>JavaScript</td>
+                            <td>2/17/21</td>
+                            <td>95</td>
+                        </tr>
+                        <tr>
+                            <td>Q4</td>
+                            <td>React</td>
+                            <td>2/24/21</td>
+                            <td>85</td>
+                        </tr>
+                        <tr>
+                            <td>Q5</td>
+                            <td>NodeJS</td>
+                            <td>3/3/21</td>
+                            <td>90</td>
+                        </tr>
+                        <tr>
+                            <td>Q6</td>
+                            <td>ExpressJS</td>
+                            <td>3/10/21</td>
+                            <td>95</td>
+                        </tr>
+                        <tr>
+                            <td>Q7</td>
+                            <td>MongoDB</td>
+                            <td>3/17/21</td>
+                            <td>85</td>
+                        </tr>
+                        <tr>
+                            <td>Q8</td>
+                            <td>GraphQL</td>
+                            <td>3/24/21</td>
+                            <td>90</td>
+                        </tr>
+                        <tr>
+                            <td>Q9</td>
+                            <td>Redux</td>
+                            <td>3/31/21</td>
+                            <td>95</td>
+                        </tr>
+                        <tr>
+                            <td>Q10</td>
+                            <td>TypeScript</td>
+                            <td>4/7/21</td>
+                            <td>85</td>
+                        </tr>
                     </tbody>
                     <tfoot>
                         <tr>
--- src/Labs/index.tsx
+++ src/Labs/index.tsx
@@ -7,7 +7,7 @@ export default function Labs() {
     return (
         <div>
             <h1>Labs</h1>
-            <h2>Jiahuan He</h2>
+            <h2>Jiahuan He From CS5610 Summer2 2025</h2>
             <TOC />
             <Routes>
                 <Route path="/" element={<Navigate to="Lab1" />} />
