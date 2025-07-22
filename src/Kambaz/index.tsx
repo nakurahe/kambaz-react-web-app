b82b5eb20d31453e2c89486ddbb60091cc49e333
Nakura <126201955+nakurahe@users.noreply.github.com>
2025-07-23 00:50:55 +0900

    fix: fixed  didn't display problem by adding a router

--- src/Kambaz/index.tsx
+++ src/Kambaz/index.tsx
@@ -14,6 +14,7 @@ export default function Kambaz() {
                     <Route path="/" element={<Navigate to="Account" />} />
                     <Route path="/Account/*" element={<Account />} />
                     <Route path="/Dashboard" element={<Dashboard />} />
+                    <Route path="/Courses" element={<Navigate to="/Kambaz/Courses/RS101/Home" />} />
                     <Route path="/Courses/:cid/*" element={<Courses />} />
                     <Route path="Calendar" element={<h1>Calendar</h1>} />
                     <Route path="Inbox" element={<h1>Inbox</h1>} />
