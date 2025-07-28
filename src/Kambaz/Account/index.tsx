59d20bb2543d2c78bebaea66eada0eb9040f72a4
Nakura <126201955+nakurahe@users.noreply.github.com>
2025-07-28 13:24:43 +0900

    feat: completed 4.4.5

--- src/Kambaz/Account/index.tsx
+++ src/Kambaz/Account/index.tsx
@@ -4,7 +4,10 @@ import AccountNavigation from "./Navigation";
 import Profile from "./Profile";
 import Signup from "./Signup";
 import { FaAlignJustify } from "react-icons/fa";
+import { useSelector } from "react-redux";
+
 export default function Account() {
+    const { currentUser } = useSelector((state: any) => state.accountReducer);
     return (
         <div id="wd-account-screen">
             <h2 className="text-danger">
@@ -18,7 +21,7 @@ export default function Account() {
                 </div>
                 <div className="flex-fill">
                     <Routes>
-                        <Route path="/" element={<Navigate to="/Kambaz/Account/Signin" />} />
+                        <Route path="/" element={<Navigate to={ currentUser ? "/Kambaz/Account/Profile" : "/Kambaz/Account/Signin" }/>} />
                         <Route path="/Signin" element={<Signin />} />
                         <Route path="/Profile" element={<Profile />} />
                         <Route path="/Signup" element={<Signup />} />
