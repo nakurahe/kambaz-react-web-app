810b9bf73d570cb0700aff6f0883c2ebc235547a
Nakura <126201955+nakurahe@users.noreply.github.com>
2025-08-04 23:45:57 -0700

    feat: tried creating modules for a course but had a bug to fix

--- src/Kambaz/Courses/Modules/index.tsx
+++ src/Kambaz/Courses/Modules/index.tsx
@@ -3,27 +3,39 @@ import ModulesControls from "./ModulesControls";
 import { FormControl, ListGroup } from "react-bootstrap";
 import ModulesControlButtons from "./ModuleControlButtons";
 import { useParams } from "react-router";
-import { useState } from "react";
-import { addModule, editModule, updateModule, deleteModule } from "./reducer";
+import { useState, useEffect } from "react";
+import { setModules, addModule, editModule, updateModule, deleteModule } from "./reducer";
 import { useSelector, useDispatch } from "react-redux";
+import * as coursesClient from "../client";
 
 export default function Modules() {
     const { cid } = useParams();
+    const dispatch = useDispatch();
     const [moduleName, setModuleName] = useState("");
     const { modules } = useSelector((state: any) => state.modulesReducer);
-    const dispatch = useDispatch();
     const { currentUser } = useSelector((state: any) => state.accountReducer);
+
+    const fetchModules = async () => {
+        const modules = await coursesClient.findModulesForCourse(cid as string);
+        dispatch(setModules(modules));
+    };
+    useEffect(() => {
+        fetchModules();
+    }, []);
+
+    const createModuleForCourse = async () => {
+        if (!cid) return;
+        const newModule = { name: moduleName, course: cid };
+        const module = await coursesClient.createModuleForCourse(cid, newModule);
+        dispatch(addModule(module));
+    };
 
     return (
         <div className="wd-modules">
             <ModulesControls setModuleName={setModuleName} moduleName={moduleName}
-            addModule={() => {
-                dispatch(addModule({ name: moduleName, course: cid }));
-                setModuleName("");
-            }} /><br /><br /><br /><br />
+            addModule={createModuleForCourse} /><br /><br /><br /><br />
             <ListGroup className="rounded-0" id="wd-modules">
                 {modules
-                    .filter((module: any) => module.course === cid)
                     .map((module: any) => (
                         <ListGroup.Item className="wd-module p-0 mb-5 fs-5 border-gray">
                             <div className="wd-title p-3 ps-2 bg-secondary">
