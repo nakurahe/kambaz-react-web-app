adb6d5783edc9ac8f9278ceec7edfb7fb4ba73bf
Nakura <126201955+nakurahe@users.noreply.github.com>
2025-08-13 17:13:21 -0700

    chore: set show my courses as default

--- src/Kambaz/Enrollments/reducer.tsx
+++ src/Kambaz/Enrollments/reducer.tsx
@@ -3,7 +3,7 @@ import { v4 as uuidv4 } from "uuid";
 
 const initialState = {
     enrollments: [],
-    showAllCourses: true,
+    showAllCourses: false,
 };
 
 const enrollmentsSlice = createSlice({
